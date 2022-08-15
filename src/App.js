92c5344dffa83d9b068cff7bcc56853fc2f6b727
DevOmid <omidpirdehi@gmail.com>
2022-08-15 11:45:03 +0430

    new update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home'
+import Header from './components/header'
+import Footer from './components/footer'
+
+function App() {
+  return (
+   <div className='App'>
+    <Header />
+    <Home />
+    <Footer />
+
+ 
+   </div>
+  );
+}
+
+export default App;
